36c94100c9e012e1c5e3fdb9dd7f2bbfcbf7ab86
Diego Lugo <diego.lugo.06@gmail.com>
2015-11-18 23:43:29 -0500

    Change :integer to :decimal for all amount fields. resolve #20

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119042421) do
+ActiveRecord::Schema.define(version: 20151119043634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,11 +31,11 @@ ActiveRecord::Schema.define(version: 20151119042421) do
 
   create_table "loans", force: :cascade do |t|
     t.integer  "student_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
     t.string   "application_status"
     t.text     "notes"
-    t.integer  "amount"
+    t.decimal  "amount",             precision: 10, scale: 2
     t.string   "company"
   end
 
@@ -85,9 +85,9 @@ ActiveRecord::Schema.define(version: 20151119042421) do
 
   create_table "wires", force: :cascade do |t|
     t.integer  "student_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "amount"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.decimal  "amount",        precision: 10, scale: 2
     t.date     "send_date"
     t.date     "received_date"
     t.string   "sender"
