fb78ba5dddaacf8224f3ec2d3f4484517988a321
Diego Lugo <diego.lugo.06@gmail.com>
2015-11-24 19:40:40 -0500

    Create filtered views for instructors. fix #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :check_admin, only: [:edit, :update, :new, :create, :destroy]
+
+  private
+  def check_admin
+    if current_user.admin? == false
+      redirect_to students_path, notice: "You are not authorized to take that action"
+    end
+  end
 end
