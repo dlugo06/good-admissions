be5af6b7c14848f7e19f258cbb90ef062ce75256
Diego Lugo <diego.lugo.06@gmail.com>
2015-11-27 23:24:26 -0500

    Create all payments index. fix #67

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,7 @@ class StudentsController < ApplicationController
   before_filter :set_cohort, only: :index
   before_filter :set_location, only: :index
   before_filter :default_filter, only: :index
-  
+
   def payments
   end
 
