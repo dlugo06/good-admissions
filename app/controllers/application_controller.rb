839174153250263a312ba19f05c82061610cc604
Diego Lugo <diego.lugo.06@gmail.com>
2015-11-30 14:49:28 -0500

    Fix instructor log out through application controller fix #19

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
-  before_action :check_admin, only: [:edit, :update, :new, :create, :destroy]
-
-  private
-  def check_admin
-    if current_user.try(:admin?) == false
-      redirect_to root_path, notice: "You are not authorized to take that action"
-    end
-  end
 end
